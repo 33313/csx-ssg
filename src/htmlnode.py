b794bf81a05c1c7e4180a48e158d0ae2342faedf
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-03-13 12:16:53 +0100

    feat: ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -31,3 +31,23 @@ class LeafNode(HTMLNode):
         if self.props is None:
             return f"<{self.tag}>{self.value}</{self.tag}>"
         return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag or None, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ERROR: Parent node requires a tag.")
+        if self.children == [] or self.children is None:
+            raise ValueError("ERROR: Parent node has no children.")
+
+        s = ""
+        for x in self.children:
+            s += x.to_html()
+        if self.props is None:
+            return f"<{self.tag}>{s}</{self.tag}>"
+        return f"<{self.tag} {self.props_to_html()}>{s}</{self.tag}>"
+
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,9 +1,9 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
         node = HTMLNode(None, None, None, {"href": "https://www.google.com"})
         self.assertEqual(node.props_to_html(), 'href="https://www.google.com"')
@@ -12,7 +12,34 @@ class TestTextNode(unittest.TestCase):
         node = LeafNode("p", "Testing")
         node2 = LeafNode("a", "Testing link", None, {"href": "https://www.google.com"})
         self.assertEqual(node.to_html(), "<p>Testing</p>")
-        self.assertEqual(node2.to_html(), '<a href="https://www.google.com">Testing link</a>')
+        self.assertEqual(
+            node2.to_html(), '<a href="https://www.google.com">Testing link</a>'
+        )
+
+    def test_parent(self):
+        leaf_list = [
+            LeafNode("p", "Testing"),
+            LeafNode("a", "Testing link", None, {"href": "https://www.google.com"}),
+        ]
+        node = ParentNode("div", leaf_list)
+        self.assertEqual(
+            node.to_html(),
+            '<div><p>Testing</p><a href="https://www.google.com">Testing link</a></div>',
+        )
+        
+        node2 = ParentNode("div", [LeafNode("p", "Hi mom")], {"test": "hello"})
+        leaf_list2 = [
+            LeafNode("p", "Testing"),
+            LeafNode("a", "Testing link", None, {"href": "https://www.google.com"}),
+            node2
+        ]
+
+        node = ParentNode("div", leaf_list2)
+
+        self.assertEqual(
+            node.to_html(),
+            '<div><p>Testing</p><a href="https://www.google.com">Testing link</a><div test="hello"><p>Hi mom</p></div></div>',
+        )
 
 
 if __name__ == "__main__":

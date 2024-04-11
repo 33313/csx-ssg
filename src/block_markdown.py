1c0a4f23bcbbe19aad452d73607c33b574f3df6d
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-04-11 18:27:48 +0200

    feat: determine md block types

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,6 @@
+from enum import Enum
 import re
+
 
 def markdown_to_blocks(md):
     rx = r"\n{2,}"
@@ -6,3 +8,66 @@ def markdown_to_blocks(md):
     for x in res:
         x = x.strip()
     return res
+
+
+BlockType = Enum(
+    "BlockType",
+    ["HEADING", "CODE", "QUOTE", "UNORDERED_LIST", "ORDERED_LIST", "PARAGRAPH"],
+)
+
+
+def block_to_block_type(block):
+    if is_heading(block):
+        return BlockType.HEADING
+    elif is_code(block):
+        return BlockType.CODE
+    elif is_quote(block):
+        return BlockType.QUOTE
+    elif is_unordered_list(block):
+        return BlockType.UNORDERED_LIST
+    elif is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+
+
+# Helper functions
+def is_heading(block):
+    return re.search(r"^#{1,6} ", block) is not None
+
+
+def is_code(block):
+    return re.search(r"^`{3}.*`{3}$", block) is not None
+
+
+def is_quote(block):
+    lines = block.split("\n")
+    for line in lines:
+        if re.search(r"^>", line) is None:
+            return False
+    return True
+
+
+def is_unordered_list(block):
+    lines = block.split("\n")
+    for line in lines:
+        if re.search(r"^(\*|\-)", line) is None:
+            return False
+    return True
+
+
+def is_ordered_list(block):
+    lines = block.split("\n")
+    last = 0
+    for line in lines:
+        search = re.search(r"^[0-9]+\.", line)
+        if search is None:
+            return False
+        num = int(search.group()[:-1])
+        if last == 0 and num != 1:
+            return False
+        elif num != last + 1:
+            return False
+        else:
+            last = num
+    return True
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,6 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import BlockType, block_to_block_type, markdown_to_blocks
+
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -17,6 +18,46 @@ This is the same paragraph on a new line
             "* This is a list\n* with items",
         ]
         self.assertEqual(res, eq)
+
+    def test_block_type_heading(self):
+        valid = "# This is a heading"
+        invalid = "####### This is not a heading"
+        res_valid = block_to_block_type(valid)
+        res_invalid = block_to_block_type(invalid)
+        self.assertEqual(res_valid, BlockType.HEADING)
+        self.assertEqual(res_invalid, BlockType.PARAGRAPH)
+
+    def test_block_type_code(self):
+        valid = "```This is a code block```"
+        invalid = "``This is not a code block``"
+        res_valid = block_to_block_type(valid)
+        res_invalid = block_to_block_type(invalid)
+        self.assertEqual(res_valid, BlockType.CODE)
+        self.assertEqual(res_invalid, BlockType.PARAGRAPH)
+
+    def test_block_type_quote(self):
+        valid = ">This is a quote.\n>This is still a quote.\n>Yep, looks like a quote to me!"
+        invalid = ">This could be a quote;\nAlas, it is not.\n>How unfortunate."
+        res_valid = block_to_block_type(valid)
+        res_invalid = block_to_block_type(invalid)
+        self.assertEqual(res_valid, BlockType.QUOTE)
+        self.assertEqual(res_invalid, BlockType.PARAGRAPH)
+
+    def test_block_type_unordered_list(self):
+        valid = "* This is a list\n- It isn't ordered\n*It works just fine."
+        invalid = "* This could've been a list.\nAlas, it is not.\n-A tragedy."
+        res_valid = block_to_block_type(valid)
+        res_invalid = block_to_block_type(invalid)
+        self.assertEqual(res_valid, BlockType.UNORDERED_LIST)
+        self.assertEqual(res_invalid, BlockType.PARAGRAPH)
+
+    def test_block_type_ordered_list(self):
+        valid = "1.In a world filled with\n2. misery and uncertainty\n3. it is a great comfort"
+        invalid = "3. To know that\n4. In the end\n5. There is a light in the darkness."
+        res_valid = block_to_block_type(valid)
+        res_invalid = block_to_block_type(invalid)
+        self.assertEqual(res_valid, BlockType.ORDERED_LIST)
+        self.assertEqual(res_invalid, BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
